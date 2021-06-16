6513c6d1d7d2c9c3f0ca3ecfb07b89babf61c32b
Shiv Gupta <18354771+shiv-io@users.noreply.github.com>
2021-06-16 18:18:53 +0530

    Generate app.db using CSVs in repo

---
+++ loop.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Delete existing app.db
+rm -f app.db
+
+# Loop through CSVs in directory
+for filename in *.csv; do
+    # Table name is the file name without the extension
+    TABLENAME=$(echo $filename | cut -d'.' -f 1)
+    
+    # Insert CSV into app.db
+    echo "Inserting $filename into app.db as table $TABLENAME"
+    sqlite-utils insert app.db $TABLENAME $filename --sniff
+done
